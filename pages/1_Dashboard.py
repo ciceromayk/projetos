c311078875bcc6fefd378b21a4265c628564c5ff
MrCic3ro <38115321+ciceromayk@users.noreply.github.com>
2026-02-25 11:06:06 -0300

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -12,8 +12,12 @@ df = load_status()
 
 progress = df.groupby("projeto")["status"].mean() / 4
 
+progress = progress.sort_values(ascending=False)
+
 for projeto, valor in progress.items():
 
     st.subheader(projeto)
 
     st.progress(float(valor))
+
+    st.write(f"{round(valor*100)} % concluído")
